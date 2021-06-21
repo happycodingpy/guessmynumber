bdad57eb7ea13fb9813597957f4d005977f8c510
happycodingpy <hugo.orlando2030@gmail.com>
2021-06-21 15:27:00 -0300

    guess my number

---
+++ script.js
@@ -0,0 +1,80 @@
+'use strict';
+
+const message = document.querySelector('.message');
+const number = document.querySelector('.number');
+const score = document.querySelector('.score');
+const guess = document.querySelector('.guess');
+const checkButton = document.querySelector('.check');
+const againButton = document.querySelector('.again');
+const myHighscore = document.querySelector('.highscore');
+
+const body = document.querySelector('body');
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let myScore = 20;
+let highscore = 0;
+
+checkButton.addEventListener('click', guessMyNumber);
+againButton.addEventListener('click', againReset);
+
+const displayMessage = msg => {
+  message.textContent = msg;
+};
+
+const guessSecNum = (guess, secretNumber) =>
+  guess > secretNumber ? '🙊 Very high!' : '🙊 Very low!';
+
+const displayScore = myScore => {
+  score.textContent = myScore;
+};
+
+const hideSecreteNumber = secNum => (number.textContent = secNum);
+
+const displayHighScore = highscore => {
+  myHighscore.textContent = highscore;
+};
+
+function guessMyNumber() {
+  const guessNum = Number(guess.value);
+
+  // verificamos si hay un valor o no
+  // !guessNum siempre sera falso
+  // por lo que si no hay un number damos un mensaje
+  if (!guessNum) {
+    displayMessage('🙈 No number!');
+  } else if (guessNum === secretNumber) {
+    displayMessage('🐵 Number correct!');
+    number.textContent = secretNumber;
+    body.style.backgroundColor = '#7952B3';
+    number.style.width = '30rem';
+
+    if (myScore > highscore) {
+      highscore = myScore;
+      //   myHighscore.textContent = highscore;
+      displayHighScore(highscore);
+    }
+  } else if (guessNum !== secretNumber) {
+    if (myScore > 1) {
+      displayMessage(guessSecNum(guessNum, secretNumber));
+      myScore--;
+      displayScore(myScore);
+    } else {
+      displayMessage('🐒 You lost the game!');
+      score.textContent = 0;
+      body.style.backgroundColor = '#301B3F';
+      displayScore(0);
+    }
+  }
+}
+
+function againReset() {
+  myScore = 20;
+  hideSecreteNumber(secretNumber);
+  hideSecreteNumber('?');
+  score.textContent = myScore;
+  score.textContent = 0;
+  guess.value = '';
+  displayMessage('Start guessing...');
+  body.style.backgroundColor = '#161616';
+  number.style.width = '30rem';
+}
